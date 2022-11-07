8b8803767c1fffcd31a8424db080d68400b9d7b3
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-07 21:52:36 +0530

    Update Print Reverse.java

--- Easy/Print Reverse.java
+++ Easy/Print Reverse.java
@@ -1,5 +1,21 @@
-// Take N as input, Calculate it's reverse also Print the reverse.
+/* 
+Take N as input, Calculate it's reverse also Print the reverse.
 
+Input Format
+
+Constraints
+0 <= N <= 1000000000
+
+Output Format
+
+Sample Input
+123456789
+Sample Output
+987654321
+
+Explanation
+You've to calculate the reverse in a number, not just print the reverse.
+*/
 
 import java.util.*;
 public class Main {
