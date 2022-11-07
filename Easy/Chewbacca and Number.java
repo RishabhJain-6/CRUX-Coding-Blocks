c85c6c01bb07ca09948fbcdc6a681ba83154e15c
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-07 21:54:01 +0530

    Update Chewbacca and Number.java

--- Easy/Chewbacca and Number.java
+++ Easy/Chewbacca and Number.java
@@ -5,9 +5,23 @@ Inverting digit t means replacing it with digit 9 - t.
 Help Chewbacca to transform the initial number x to the minimum possible positive number by inverting some (possibly, zero) digits. 
 The decimal representation of the final number shouldn't start with a zero.
 
+Input Format
+The first line contains a single integer x (1 ≤ x ≤ 10^18) — the number that Luke Skywalker gave to Chewbacca.
+
 Constraints
 x <= 100000000000000000
 
+Output Format
+Print the minimum possible positive number that Chewbacca can obtain after inverting some digits. The number shouldn't contain leading zeroes.
+
+Sample Input
+4545
+Sample Output
+4444
+
+Explanation
+There are many numbers form after inverting the digit. For minimum number, check if inverting digit is less than or greater than the original digit. 
+If it is less, then invert it otherwise leave it.
 */
 
 
