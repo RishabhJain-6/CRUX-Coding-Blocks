df2e5c2cee9c09f397b9abf124393e46120203b0
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-07 22:15:29 +0530

    Create Lower Upper.java

---
+++ Easy/Lower Upper.java
@@ -0,0 +1,36 @@
+/*
+Print "lowercase" if user enters a character between 'a-z' , Print "UPPERCASE" is character lies between 'A-Z' and 
+print "Invalid" for all other characters like $,.^# etc.
+
+Input Format
+Single Character .
+
+Constraints
+-
+
+Output Format
+lowercase UPPERCASE Invalid
+
+Sample Input
+$
+Sample Output
+Invalid
+
+Explanation
+-
+*/
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+		    char ch = scn.next().charAt(0);
+		    if (ch >= 'a' && ch <= 'z') {
+			    System.out.println("lowercase");
+		    } else if (ch >= 'A' && ch <= 'Z') {
+			    System.out.println("UPPERCASE");
+		    } else {
+			    System.out.println("Invalid");
+		    }
+    }
+}
