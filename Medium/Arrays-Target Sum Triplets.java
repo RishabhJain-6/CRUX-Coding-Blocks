2dd3bac1b54e41a2d9a0c08e4f6258ac8c2198aa
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 10:45:14 +0530

    Create Arrays-Target Sum Triplets.java

---
+++ Medium/Arrays-Target Sum Triplets.java
@@ -0,0 +1,53 @@
+/*
+Take as input N, the size of array. Take N more inputs and store that in an array. 
+Take as input “target”, a number. Write a function which prints all triplets of numbers which sum to target.
+
+Input Format
+First line contains input N.
+Next line contains N space separated integers denoting the elements of the array.
+The third line contains a single integer T denoting the target element.
+
+Constraints
+Length of Array should be between 1 and 1000.
+
+Output Format
+Print all the triplet present in the array in a new line each. 
+The triplets must be printed as A, B and C where A,B and C are the elements of the triplet ( A<=B<=C) and all triplets must be printed in sorted order. 
+Print only unique triplets.
+
+Sample Input
+9
+5 7 9 1 2 4 6 8 3
+10
+Sample Output
+1, 2 and 7
+1, 3 and 6
+1, 4 and 5
+2, 3 and 5
+
+Explanation
+Array = {5, 7, 9, 1, 2, 4, 6 ,8 ,3}. Target number = 10. Find any three number in the given array which sum to target number.
+*/
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int size = scn.nextInt();
+		int[] arr = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = scn.nextInt();
+		}
+		int target = scn.nextInt();
+		Arrays.sort(arr);
+		for (int i = 0; i < size; i++) {
+			for (int j = i + 1; j < size; j++) {
+				for (int k = j + 1; k < size; k++) {
+					if (arr[i] + arr[j] + arr[k] == target) {
+						System.out.println(arr[i] + ", " + arr[j] + " and " + arr[k]);
+					}
+				}
+			}
+		}
+	}
+}
