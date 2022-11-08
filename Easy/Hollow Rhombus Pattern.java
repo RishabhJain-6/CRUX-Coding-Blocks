6707a6536179be1b4946b32e2716ada016697944
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 15:17:54 +0530

    Update Hollow Rhombus Pattern.java

--- Easy/Hollow Rhombus Pattern.java
+++ Easy/Hollow Rhombus Pattern.java
@@ -27,32 +27,32 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		    int n = scn.nextInt();
-		    int numsp = n - 1;
-		    int numst = n;
-		    int row = 1;
-		    while (row <= n) {
-			    int s = 1;
-			    while (s <= numsp) {
-				    System.out.print(" ");
-				    s++;
-			    }
-			    int i = 1;
-			    while (i <= numst) {
-				    if (row > 1 && row < n) {
-					    if (i == 1 || i == numst) {
-						    System.out.print("*");
-					    } else {
-						    System.out.print(" ");
-					    }
-				    } else {
-					    System.out.print("*");
-				    }
-				    i++;
-			    }
-			    numsp--;
-			    row++;
-			    System.out.println();
-		    }
+	int n = scn.nextInt();
+	int numsp = n - 1;
+	int numst = n;
+	int row = 1;
+	while (row <= n) {
+		int s = 1;
+		while (s <= numsp) {
+			System.out.print(" ");
+			s++;
+		}
+		int i = 1;
+		while (i <= numst) {
+			if (row > 1 && row < n) {
+				if (i == 1 || i == numst) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			} else {
+				System.out.print("*");
+			}
+			i++;
+		}
+		numsp--;
+		row++;
+		System.out.println();
+	}
     }
 }
