643b7bbbe10a5e1244497d873d26850c684dd707
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 15:11:18 +0530

    Update Pattern Magic.java

--- Easy/Pattern Magic.java
+++ Easy/Pattern Magic.java
@@ -30,44 +30,44 @@ import java.util.*;
 public class Main {
     public static void main (String args[]) {
         Scanner scn = new Scanner(System.in);
-		    int n = scn.nextInt();
-		    int row = 1;
-		    int numst1 = n;
-		    int numst2 = n-1;
-		    int numsp = 0;
-		    while (row <= 2*n-1) {
-			    int i = 1;
-			    while (i <= numst1) {
-				    System.out.print("*");
-				    i++;
-			    }
-			    int s = 1;
-			    while (s <= numsp) {
-				    System.out.print(" ");
-				    s++;
-			    }
-			    int j = 1;
-			    while (j <= numst2) {
-				    System.out.print("*");
-				    j++;
-			    }
-			    if (row == 1) {
-				    numst1--;
-				    numsp++;
-			    } else if (row < n) {
-				    numst1--;
-				    numst2--;
-				    numsp += 2;
-			    } else if (row == 2*n - 2) {
-				    numst1++;
-				    numsp -= 1;
-			    } else {
-				    numst1++;
-				    numst2++;
-				    numsp -= 2;
-			    }
-			    System.out.println();
-			    row++;
-		    }
+	int n = scn.nextInt();
+	int row = 1;
+	int numst1 = n;
+	int numst2 = n-1;
+	int numsp = 0;
+	while (row <= 2*n-1) {
+		int i = 1;
+		while (i <= numst1) {
+			System.out.print("*");
+			i++;
+		}
+		int s = 1;
+		while (s <= numsp) {
+			System.out.print(" ");
+			s++;
+		}
+		int j = 1;
+		while (j <= numst2) {
+			System.out.print("*");
+			j++;
+		}
+		if (row == 1) {
+			numst1--;
+			numsp++;
+		} else if (row < n) {
+			numst1--;
+			numst2--;
+			numsp += 2;
+		} else if (row == 2*n - 2) {
+			numst1++;
+			numsp -= 1;
+		} else {
+			numst1++;
+			numst2++;
+			numsp -= 2;
+		}
+		System.out.println();
+		row++;
+	}
     }
 }
