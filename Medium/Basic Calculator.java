a4ddafaf1820843fd49622e7c649f5220b6635c2
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 21:17:04 +0530

    Update Basic Calculator.java

--- Medium/Basic Calculator.java
+++ Medium/Basic Calculator.java
@@ -45,42 +45,41 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		    char ch = 'a';
-		    while (ch!='x' || ch!='X')
-		    {
-			    ch = scn.next().charAt(0);
-          if (ch=='+') {
-				    int n1 = scn.nextInt();
-				    int n2 = scn.nextInt();
-				    System.out.println(n1+n2);
-			    }
-			    else if (ch=='-') {
-				    int n1 = scn.nextInt();
-				    int n2 = scn.nextInt();
-				    System.out.println(n1-n2);
-			    }
-			    else if (ch=='*') {
-				    int n1 = scn.nextInt();
-				    int n2 = scn.nextInt();
-				    System.out.println(n1*n2);
-			    }
-			    else if (ch=='/') {
-				    int n1 = scn.nextInt();
-				    int n2 = scn.nextInt();
-				    System.out.println(n1/n2);
-			    }
-			    else if (ch=='%') {
-				    int n1 = scn.nextInt();
-				    int n2 = scn.nextInt();
-				    System.out.println(n1%n2);
-			    }
-			    else if (ch=='x' || ch=='X') {
-				    break;
-			    }
-			    else
-			    {
-				    System.out.println("Invalid operation. Try again.");
-			    }
-		    }
+	char ch = 'a';
+	while (ch!='x' || ch!='X')
+	{
+		ch = scn.next().charAt(0);
+          	if (ch=='+') {
+			int n1 = scn.nextInt();
+			int n2 = scn.nextInt();
+			System.out.println(n1+n2);
+		}
+		else if (ch=='-') {
+			int n1 = scn.nextInt();
+			int n2 = scn.nextInt();
+			System.out.println(n1-n2);
+		}
+		else if (ch=='*') {
+			int n1 = scn.nextInt();
+			int n2 = scn.nextInt();
+			System.out.println(n1*n2);
+		}
+		else if (ch=='/') {
+			int n1 = scn.nextInt();
+			int n2 = scn.nextInt();
+			System.out.println(n1/n2);
+		}
+		else if (ch=='%') {
+			int n1 = scn.nextInt();
+			int n2 = scn.nextInt();
+			System.out.println(n1%n2);
+		}
+		else if (ch=='x' || ch=='X') {
+			break;
+		}
+		else {
+			System.out.println("Invalid operation. Try again.");
+		}
+	}
     }
 }
