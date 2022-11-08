806af2fa17e0b07ca206e5f5da36548572355951
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 15:02:49 +0530

    Update Simple Input.java

--- Easy/Simple Input.java
+++ Easy/Simple Input.java
@@ -26,13 +26,13 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		    int sum = 0;
-		    while (sum >= 0) {
-			    int n = scn.nextInt();
-			    sum += n;
-			    if (sum < 0)
-				    break;
-			    System.out.println(n);
-		    }
+	int sum = 0;
+	while (sum >= 0) {
+		int n = scn.nextInt();
+		sum += n;
+		if (sum < 0)
+			break;
+		System.out.println(n);
+	}
     }
 }
