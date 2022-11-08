f1deebec5c7a8ae01f9bb1f56c387d3dc04b0d58
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 15:34:40 +0530

    Update Print Primes.java

--- Medium/Print Primes.java
+++ Medium/Print Primes.java
@@ -22,23 +22,23 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		    int num = scn.nextInt();
-		    int i = 2;
-		    while (i <= num) {
-			    int n = i;
-			    int count = 0;
-			    int j = 2;
-			    while (j <= Math.sqrt(n)) {
-				    int rem = n % j;
-				    if (rem == 0) {
-					    count++;
-				    }
-				    j++;
-			    }
-			    if (count == 0 && n != 1) {
-				    System.out.println(i);
-			    }
-			    i++;
-		    }            
+	int num = scn.nextInt();
+	int i = 2;
+	while (i <= num) {
+		int n = i;
+		int count = 0;
+		int j = 2;
+		while (j <= Math.sqrt(n)) {
+			int rem = n % j;
+			if (rem == 0) {
+				count++;
+			}
+			j++;
+		}
+		if (count == 0 && n != 1) {
+			System.out.println(i);
+		}
+		i++;
+	}            
     }
 }
