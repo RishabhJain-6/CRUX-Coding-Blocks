5f95219fdc9884f691596ed2bf47278d7c4d138d
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 21:07:16 +0530

    Update Mirror Star Pattern(Pattern 5).java

--- Medium/Mirror Star Pattern(Pattern 5).java
+++ Medium/Mirror Star Pattern(Pattern 5).java
@@ -30,30 +30,30 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		    int n = scn.nextInt();
-		    int numsp = n / 2;
-		    int numst = 1;
-		    int row = 1;
-		    while (row <= n) {
-			    int s = 1;
-			    while (s <= numsp) {
-				    System.out.print("\t");
-				    s++;
-			    }
-			    int i = 1;
-			    while (i <= numst) {
-				    System.out.print("*\t");
-				    i++;
-			    }
-			    if (row < n / 2 + 1) {
-				    numsp--;
-				    numst += 2;
-			    } else {
-				    numsp++;
-				    numst -= 2;
-			    }
-			    row++;
-			    System.out.println();
-		    }
+	int n = scn.nextInt();
+	int numsp = n / 2;
+	int numst = 1;
+	int row = 1;
+	while (row <= n) {
+		int s = 1;
+		while (s <= numsp) {
+			System.out.print("\t");
+			s++;
+		}
+		int i = 1;
+		while (i <= numst) {
+			System.out.print("*\t");
+			i++;
+		}
+		if (row < n / 2 + 1) {
+			numsp--;
+			numst += 2;
+		} else {
+			numsp++;
+			numst -= 2;
+		}
+		row++;
+		System.out.println();
+	}
     }
 }
