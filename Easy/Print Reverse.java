55c32a4a31667ceffeddd153e3072c069d8f7ad3
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 15:08:51 +0530

    Update Print Reverse.java

--- Easy/Print Reverse.java
+++ Easy/Print Reverse.java
@@ -21,13 +21,13 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		int num = scn.nextInt();
-		int rev = 0;
-		while (num != 0) {
-			int digit = num % 10;
-			rev = (rev * 10) + digit;
-			num = num / 10;
-		}
-		System.out.println(rev);
+	int num = scn.nextInt();
+	int rev = 0;
+	while (num != 0) {
+		int digit = num % 10;
+		rev = (rev * 10) + digit;
+		num = num / 10;
+	}
+	System.out.println(rev);
     }
 }
