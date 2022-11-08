d0a267a51dad771a5aa2ee41308aa7b702a52100
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 15:14:42 +0530

    Update Lower Upper.java

--- Easy/Lower Upper.java
+++ Easy/Lower Upper.java
@@ -24,13 +24,13 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		    char ch = scn.next().charAt(0);
-		    if (ch >= 'a' && ch <= 'z') {
-			    System.out.println("lowercase");
-		    } else if (ch >= 'A' && ch <= 'Z') {
-			    System.out.println("UPPERCASE");
-		    } else {
-			    System.out.println("Invalid");
-		    }
+	char ch = scn.next().charAt(0);
+	if (ch >= 'a' && ch <= 'z') {
+		System.out.println("lowercase");
+	} else if (ch >= 'A' && ch <= 'Z') {
+		System.out.println("UPPERCASE");
+	} else {
+		System.out.println("Invalid");
+	}
     }
 }
