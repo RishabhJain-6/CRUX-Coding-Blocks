d37011ea517b8d94267d60b6a44ca5022428bb1c
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 21:05:58 +0530

    Update Nth Fibonacci.java

--- Medium/Nth Fibonacci.java
+++ Medium/Nth Fibonacci.java
@@ -21,16 +21,16 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		    int n = scn.nextInt();
-		    int i=1;
-		    int a = 0;
-		    int b = 1;
-		    while (i<=n) {
-			    int c = a + b;
-			    a = b;
-			    b = c;
-			    i++;
-		    }
-		    System.out.println(a);
+	int n = scn.nextInt();
+	int i=1;
+	int a = 0;
+	int b = 1;
+	while (i<=n) {
+		int c = a + b;
+		a = b;
+		b = c;
+		i++;
+	}
+	System.out.println(a);
     }
 }
