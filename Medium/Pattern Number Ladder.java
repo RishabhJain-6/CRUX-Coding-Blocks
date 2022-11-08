110ed9d39df09724287f5c7aa736678e8d306e59
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 10:29:53 +0530

    Create Pattern Number Ladder.java

---
+++ Medium/Pattern Number Ladder.java
@@ -0,0 +1,46 @@
+/*
+Take N (number of rows), print the following pattern (for N = 4)
+
+1
+2 3
+4 5 6
+7 8 9 10
+
+Input Format
+
+Constraints
+0 < N < 100
+
+Output Format
+
+Sample Input
+4
+Sample Output
+1  
+2	3  
+4	5	6  
+7	8	9	10
+
+Explanation
+Each number is separated from other by a tab.
+*/
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+		    int n = scn.nextInt();
+		    int row = 1;
+		    int a = 1;
+		    while (row <= n) {
+			    int i = 1;
+			    while (i <= row) {
+				    System.out.print(a + "\t");
+				    a++;
+				    i++;
+			    }
+			    row++;
+			    System.out.println();
+		    }
+    }
+}
