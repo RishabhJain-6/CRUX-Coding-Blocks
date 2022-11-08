53690687243c8c25caa928ee8a893538a6060c8d
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 21:13:30 +0530

    Update Conversion(Fahrenheit to Celsius).java

--- Medium/Conversion(Fahrenheit to Celsius).java
+++ Medium/Conversion(Fahrenheit to Celsius).java
@@ -43,12 +43,12 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		    int min = scn.nextInt();
-		    int max = scn.nextInt();
-		    int step = scn.nextInt();
-		    for (int i = min; i <= max; i += step) {
-			    int c = (5 * i - 160) / 9;
-			    System.out.println(i + "\t" + c);
-		    }
+	int min = scn.nextInt();
+	int max = scn.nextInt();
+	int step = scn.nextInt();
+	for (int i = min; i <= max; i += step) {
+		int c = (5 * i - 160) / 9;
+		System.out.println(i + "\t" + c);
+	}
     }
 }
