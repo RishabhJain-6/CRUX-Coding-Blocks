7a45b3904aac35ea9cc7fd52329eadc30df2dc8f
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 15:45:52 +0530

    Update Pattern Rhombus.java

--- Medium/Pattern Rhombus.java
+++ Medium/Pattern Rhombus.java
@@ -16,7 +16,7 @@ Output Format
 Sample Input
 3
 Sample Output
-    1
+    		1		
 	2	3	2
 3	4	5	4	3
 	2	3	2
@@ -30,37 +30,37 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		    int n = scn.nextInt();
-		    int row = 1;
-		    int numsp = n - 1;
-		    int numst = 1;
-		    int a = 1;
-		    while (row <= 2 * n - 1) {
-			    int s = 1;
-			    while (s <= numsp) {
-				    System.out.print("\t");
-				    s++;
-			    }
-			    int i = 1;
-			    while (i <= numst) {
-				    System.out.print(a + "\t");
-				    if (i <= numst / 2) {
-					    a++;
-				    } else {
-					    a--;
-				    }
-				    i++;
-			    }
-			    if (row < n) {
-				    numsp--;
-				    numst += 2;
-            a+=2;
-			    } else {
-				    numsp++;
-				    numst -= 2;
-			    }
-			    row++;
-			    System.out.println();
-		    }
+	int n = scn.nextInt();
+	int row = 1;
+	int numsp = n - 1;
+	int numst = 1;
+	int a = 1;
+	while (row <= 2 * n - 1) {
+		int s = 1;
+		while (s <= numsp) {
+			System.out.print("\t");
+			s++;
+		}
+		int i = 1;
+		while (i <= numst) {
+			System.out.print(a + "\t");
+			if (i <= numst / 2) {
+				a++;
+			} else {
+				a--;
+			}
+			i++;
+		}
+		if (row < n) {
+			numsp--;
+			numst += 2;
+            		a+=2;
+		} else {
+			numsp++;
+			numst -= 2;
+		}
+		row++;
+		System.out.println();
+	}
     }
 }
