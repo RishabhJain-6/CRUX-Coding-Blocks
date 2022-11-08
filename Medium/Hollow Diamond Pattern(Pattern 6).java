c2b369c4ab0148b33f21c1f1e3db2ee404c6500a
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2022-11-08 21:11:34 +0530

    Update Hollow Diamond Pattern(Pattern 6).java

--- Medium/Hollow Diamond Pattern(Pattern 6).java
+++ Medium/Hollow Diamond Pattern(Pattern 6).java
@@ -15,11 +15,11 @@ Output Format
 Sample Input
 5
 Sample Output
-* *	* * *		  
-*	*	  * *
-* 	    *
+* * * * *
 * *   * *
-* *	* * *	
+*       *
+* *   * *
+* * * * *
 
 Explanation
 Each '*' is separated from other by a tab.
@@ -29,44 +29,44 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
-		    int n = scn.nextInt();
-		    int row = 1;
-		    int numst1 = n / 2 + 1;
-		    int numst2 = n / 2;
-		    int numsp = 0;
-		    while (row <= n) {
-			    int i = 1;
-			    while (i <= numst1) {
-				    System.out.print("*\t");
-				    i++;
-			    }
-			    int s = 1;
-			    while (s <= numsp) {
-				    System.out.print(" \t");
-				    s++;
-			    }
-			    int j = 1;
-			    while (j <= numst2) {
-				    System.out.print("*\t");
-				    j++;
-			    }
-			    if (row == 1) {
-				    numst1--;
-				    numsp++;
-			    } else if (row <= n / 2) {
-				    numst1--;
-				    numst2--;
-				    numsp += 2;
-			    } else if (row == n - 1) {
-				    numst1++;
-				    numsp -= 2;
-			    } else {
-				    numst1++;
-				    numst2++;
-				    numsp -= 2;
-			    }
-			    System.out.println();
-			    row++;
-		    }
+	int n = scn.nextInt();
+	int row = 1;
+	int numst1 = n / 2 + 1;
+	int numst2 = n / 2;
+	int numsp = 0;
+	while (row <= n) {
+		int i = 1;
+		while (i <= numst1) {
+			System.out.print("*\t");
+			i++;
+		}
+		int s = 1;
+		while (s <= numsp) {
+			System.out.print(" \t");
+			s++;
+		}
+		int j = 1;
+		while (j <= numst2) {
+			System.out.print("*\t");
+			j++;
+		}
+		if (row == 1) {
+			numst1--;
+			numsp++;
+		} else if (row <= n / 2) {
+			numst1--;
+			numst2--;
+			numsp += 2;
+		} else if (row == n - 1) {
+			numst1++;
+			numsp -= 2;
+		} else {
+			numst1++;
+			numst2++;
+			numsp -= 2;
+		}
+		System.out.println();
+		row++;
+	}
     }
 }
