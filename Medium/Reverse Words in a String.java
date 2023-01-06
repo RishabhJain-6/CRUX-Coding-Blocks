169c7b0e679e9ead093996114db3f77572d79299
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2023-01-06 22:48:38 +0530

    Update Reverse Words in a String.java

--- Medium/Reverse Words in a String.java
+++ Medium/Reverse Words in a String.java
@@ -51,4 +51,27 @@ public class Main {
 			return reverse(remain, path + add + " ");
 		}
 	}
+	
+	public static String reverseWords(String s) {  //Another solution
+		int n = s.length();
+		StringBuilder ans = new StringBuilder("");
+		StringBuilder temp = new StringBuilder("");
+
+		for (int i = n - 1; i >= 0; i--) {
+			char ch = s.charAt(i);
+
+			if (ch != ' ')
+				temp.append(ch);
+
+			if ((ch == ' ' || i == 0) && temp.length() > 0) {
+				if (ans.length() > 0)
+					ans.append(" ");
+				temp.reverse();
+				ans.append(temp);
+				temp.setLength(0);
+			}
+		}
+
+		return ans.toString();
+	}
 }
