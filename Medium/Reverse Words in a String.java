89fd194f8688a471e1abb62ab5ac9f6e0bba8081
Rishabh Jain <99319023+RishabhJain-6@users.noreply.github.com>
2023-01-06 22:45:13 +0530

    Create Reverse Words in a String.java

---
+++ Medium/Reverse Words in a String.java
@@ -0,0 +1,54 @@
+/*
+Given an input string s, reverse the order of the words.
+
+Input Format
+A line containing multiple words
+
+Constraints
+The length of string <= 10^4
+
+Output Format
+The line in reversed order
+
+Sample Input
+the sky is blue
+Sample Output
+blue is sky the
+
+Explanation
+Reverse the string by reversing order of the words.
+*/
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String S = scn.nextLine();
+        while (S.charAt(0) == ' ') {
+			S = S.substring(1);
+		}
+		while (S.charAt(S.length() - 1) == ' ') {
+			S = S.substring(0, S.length() - 1);
+		}
+		System.out.println(reverse(S, ""));
+	}
+
+	public static String reverse(String S, String path) {
+		if (S.length() == 0) {
+			return path;
+		}
+
+		int a = S.lastIndexOf(' ');
+		String remain;
+		String add;
+		if (a == -1) {
+			remain = "";
+			add = S;
+			return reverse(remain, path + add);
+		} else {
+			remain = S.substring(0, a);
+			add = S.substring(a + 1);
+			return reverse(remain, path + add + " ");
+		}
+	}
+}
